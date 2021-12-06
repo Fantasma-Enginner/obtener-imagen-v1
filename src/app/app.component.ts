03b0a5ea09a89789d056c0545c817a3136e68c3a
Fantasma Enginner <ingenieroromario@gmail.com>
2021-12-06 14:48:11 -0500

    inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient,HttpEventType } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'imagenFront';
+  constructor(private httpClient: HttpClient) { }
+
+  selectedFile: any;
+  imgURL: any;
+  retrievedImage: any;
+  base64Data: any;
+  retrieveResonse: any;
+  message: any;
+  imageName: any;
+
+  public onFileChanged(event:any) {
+    //Select File
+    this.selectedFile = event.target.files[0];
+  }
+
+
+  onUpload() {
+    console.log(this.selectedFile);
+    const uploadImageData = new FormData();
+    uploadImageData.append('imageFile', this.selectedFile, this.selectedFile.name);
+
+
+    this.httpClient.post('http://localhost:8080/image/upload', uploadImageData, { observe: 'response' })
+      .subscribe((response) => {
+        if (response.status === 200) {
+          this.message = 'Image uploaded successfully';
+        } else {
+          this.message = 'Image not uploaded successfully';
+        }
+      }
+      );
+
+
+  }
+
+  getImage() {
+    this.httpClient.get('http://localhost:8080/image/get/' + this.imageName)
+      .subscribe(
+        res => {
+          this.retrieveResonse = res;
+          this.base64Data = this.retrieveResonse.picByte;
+          this.retrievedImage = 'data:image/jpeg;base64,' + this.base64Data;
+        }
+      );
+  }
 }
